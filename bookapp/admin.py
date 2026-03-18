c8d8e6e1fce7a45ff92ecaefdbfcc0ca7eead470
kaakarsha <kaakarsha28@gmail.com>
2026-03-18 05:58:19 +0000

    pylint

--- bookapp/admin.py
+++ bookapp/admin.py
@@ -1,7 +1,7 @@
+"""Dajngo Admin here"""
 from django.contrib import admin
 from bookapp import models
 
 # Register your models here.
 admin.site.register(models.BookAllotmentModel)
 admin.site.register(models.BookModel)
-
--- bookapp/apps.py
+++ bookapp/apps.py
@@ -1,6 +1,6 @@
+"""Django Apps here"""
 from django.apps import AppConfig
-
-
 class BookappConfig(AppConfig):
+    """Book App Config"""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'bookapp'
