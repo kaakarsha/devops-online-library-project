8ea9ad2e948a8139dfb165df8003344ad3246b81
kaakarsha <kaakarsha28@gmail.com>
2026-03-18 06:50:15 +0000

    sonar changes

--- keepreading/settings.py
+++ keepreading/settings.py
@@ -13,16 +13,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import os
 from django.core.management.utils import get_random_secret_key
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = get_random_secret_key()
+SECRET_KEY = "kdlk@3';>2541Xszv?'(876%^DDcvf3?"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -114,15 +107,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-
-#STATIC_URL = '/static/'
-# STATICFILES_DIRS=[ os.path.join(BASE_DIR, 'static'),]
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-
 STATIC_URL = "static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
@@ -130,9 +114,6 @@ STATIC_ROOT = "staticfiles/"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
